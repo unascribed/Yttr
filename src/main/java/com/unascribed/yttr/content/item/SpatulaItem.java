4ebc15bc544173429aa6fc9d9e378b51a432fe2d
Una Thompson <una@unascribed.com>
2022-01-19 01:35:46 -0800

    Spatula tweaks

--- src/main/java/com/unascribed/yttr/content/item/SpatulaItem.java
+++ src/main/java/com/unascribed/yttr/content/item/SpatulaItem.java
@@ -28,13 +28,13 @@ public class SpatulaItem extends ShovelItem {
 	public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
 		attacker.world.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, attacker.getSoundCategory(), 0.25f, 0.5f);
 		attacker.world.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, attacker.getSoundCategory(), 0.25f, 0.75f);
-		serve(target, attacker);
+		serve(target, attacker, 0.75f);
 		return super.postHit(stack, target, attacker);
 	}
 	
 	@Override
 	public ActionResult useOnEntity(ItemStack stack, PlayerEntity attacker, LivingEntity target, Hand hand) {
-		serve(target, attacker);
+		serve(target, attacker, 0.3f);
 		stack.damage(1, attacker, (t) -> attacker.sendEquipmentBreakStatus(hand == Hand.MAIN_HAND ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND));
 		attacker.getItemCooldownManager().set(this, 10);
 		return ActionResult.SUCCESS;
@@ -51,7 +51,7 @@ public class SpatulaItem extends ShovelItem {
 		if (bs.getHardness(w, pos) < 0) return ActionResult.FAIL;
 		if (!w.isClient) {
 			FallingBlockEntity fbe = new FallingBlockEntity(w, pos.getX()+0.5, pos.getY()+0.5, pos.getZ()+0.5, bs);
-			fbe.setVelocity(p.getRotationVector().multiply(0.5).add(0, 1, 0));
+			fbe.setVelocity(p.getRotationVector().multiply(p.isSprinting() ? 0.7 : 0.5).add(0, 1, 0));
 			p.world.playSound(null, p.getX(), p.getY(), p.getZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, p.getSoundCategory(), 1, 1.25f);
 			p.world.playSound(null, p.getX(), p.getY(), p.getZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, p.getSoundCategory(), 1, 1.75f);
 			w.playSound(null, pos, ((AccessorBlockSoundGroup)bs.getSoundGroup()).yttr$getBreakSound(), SoundCategory.BLOCKS, 0.5f, 1);
@@ -62,11 +62,11 @@ public class SpatulaItem extends ShovelItem {
 		return ActionResult.SUCCESS;
 	}
 	
-	private void serve(LivingEntity target, LivingEntity attacker) {
+	private void serve(LivingEntity target, LivingEntity attacker, float force) {
 		attacker.world.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, attacker.getSoundCategory(), 1, 1.25f);
 		attacker.world.playSound(null, attacker.getX(), attacker.getY(), attacker.getZ(), SoundEvents.ENTITY_PLAYER_ATTACK_SWEEP, attacker.getSoundCategory(), 1, 1.75f);
-		target.takeKnockback(2, attacker.getX() - target.getX(), attacker.getZ() - target.getZ());
-		target.setVelocity(target.getVelocity().add(0, 0.5, 0));
+		target.takeKnockback(0.5f+force+(attacker.isSprinting() ? 0.3f : 0), attacker.getX() - target.getX(), attacker.getZ() - target.getZ());
+		target.setVelocity(target.getVelocity().add(0, force*0.8f, 0));
 	}
 
 }
